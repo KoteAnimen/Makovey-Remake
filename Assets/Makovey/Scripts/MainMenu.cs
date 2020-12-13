d4d5d8e03289b4fab6f841146fdac55fd1638750
Kote\Kote_Animen <sverh.il@yandex.ru>
2020-12-13 21:59:22 +0300

    главное мею игры готово

---
+++ Assets/Makovey/Scripts/MainMenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+	GameObject aboutUsPanel;
+	public string nameOfGameScene;
+	public GameObject nameGame;
+	public GameObject startButton;
+	public GameObject aboutUsButton;
+	public GameObject exitButton;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        aboutUsPanel = GameObject.Find("AboutUsPanel");
+		aboutUsPanel.SetActive(false);
+    }
+	
+	public void StartGame()
+	{
+		Application.LoadLevel(nameOfGameScene);		
+	}
+	public void AboutUs(bool chose)
+	{
+		chose = !chose;
+		nameGame.SetActive(chose);
+		startButton.SetActive(chose);
+		aboutUsButton.SetActive(chose);
+		exitButton.SetActive(chose);
+		aboutUsPanel.SetActive(!chose);
+		
+	}
+	public void ExitGame()
+	{
+		Application.Quit();
+	}
+
+    
+}
